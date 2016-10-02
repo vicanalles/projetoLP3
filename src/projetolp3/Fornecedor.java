a856e1a1d179a9ca01d4ef086618b454806a8dd6
Rafael <Rafael@Chuque-NB>
2016-10-02 17:44:49 -0300

    classes: pessoa, cliente, funcionario e fornecedor

---
+++ src/projetolp3/Fornecedor.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolp3;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Fornecedor {
+    public Fornecedor()
+    {
+        
+    }
+    
+    private String cnpj;
+    private String nome;
+    private String nomeFantasia;
+    private String cep;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String complemento;
+    private String estado;
+    
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public void cadastrarFornecedor()
+    {
+        
+    }
+    public void alterarFornecedor()
+    {
+        
+    }
+    public void excluirFornecedor()
+    {
+        
+    }
+}
