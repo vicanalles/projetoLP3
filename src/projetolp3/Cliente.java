d2429ca89a2b4cd8322100a03829cf1708105002
Rafael <Rafael@Chuque-NB>
2016-10-04 21:21:13 -0300

    classes: pessoa, cliente, funcionario e fornecedor

---
+++ src/projetolp3/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolp3;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Cliente extends Pessoa {
+    public Cliente()
+    {
+        super();
+    }
+    
+    private int numeroFilhos;
+    
+    public void cadastrarCliente()
+    {
+        
+    }
+    public void alterarCliente()
+    {
+        
+    }
+    public void excluirCliente()
+    {
+        
+    }
+}
---
+++ src/projetolp3/Pedido.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetolp3;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Pedido {
+    public Pedido()
+    {
+        
+    }
+    
+    private int numero;
+    private String pagamento;
+    
+    public void cadastrarPedido()
+    {
+        
+    }
+    public void alterarPedido()
+    {
+        
+    }
+}
