06ac945b6b1217382b460ac70e00ec23dd6062d5
Rafael <Rafael@Chuque-NB>
2016-11-24 18:24:50 -0200

    correções DAO fornecedor

--- src/Model/DAO/FornecedorDAO.java
+++ src/Model/DAO/FornecedorDAO.java
@@ -55,9 +55,43 @@ public class FornecedorDAO
         }
     }
     
-    public Fornecedor selectOneByCnpj(String string)
+    public Fornecedor selectOneByCnpj(String cnpj)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "select cnpj, nome, nomeFantasia, telefone, cep, rua, numero, bairro, cidade, estado, complemento, anotacoes from fornecedor where cnpj = ?;";
+        Fornecedor fornecedor = new Fornecedor();
+        
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            
+            preparedStatement.setString(1, cnpj);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            if(resultSet.next())
+            {
+                fornecedor.setCnpj(resultSet.getString(1));
+                fornecedor.setNome(resultSet.getString(2));
+                fornecedor.setNomeFantasia(resultSet.getString(3));
+                fornecedor.setTelefone(resultSet.getString(4));
+                fornecedor.setCep(resultSet.getString(5));
+                fornecedor.setRua(resultSet.getString(6));
+                fornecedor.setNumero(resultSet.getInt(7));
+                fornecedor.setBairro(resultSet.getString(8));
+                fornecedor.setCidade(resultSet.getString(9));
+                fornecedor.setEstado(resultSet.getString(10));
+                fornecedor.setComplemento(resultSet.getString(11));
+                fornecedor.setAnotacoes(resultSet.getString(12));
+            }
+            
+            preparedStatement.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        return fornecedor;
     }
     
     public ArrayList<Fornecedor> selectByCnpj(String cnpj)
@@ -76,19 +110,19 @@ public class FornecedorDAO
             while(resultSet.next())
             {
                 Fornecedor fornecedor = new Fornecedor();
-                preparedStatement.setString(1, fornecedor.getCnpj());
-                preparedStatement.setString(2, fornecedor.getNome());
-                preparedStatement.setString(3, fornecedor.getNomeFantasia());
-                preparedStatement.setString(4, fornecedor.getTelefone());
-                preparedStatement.setString(5, fornecedor.getCep());
-                preparedStatement.setString(6, fornecedor.getRua());
-                preparedStatement.setInt(7, fornecedor.getNumero());
-                preparedStatement.setString(8, fornecedor.getBairro());
-                preparedStatement.setString(9, fornecedor.getCidade());
-                preparedStatement.setString(10, fornecedor.getEstado());
-                preparedStatement.setString(11, fornecedor.getComplemento());
-                preparedStatement.setString(12, fornecedor.getAnotacoes());
-
+                fornecedor.setCnpj(resultSet.getString(1));
+                fornecedor.setNome(resultSet.getString(2));
+                fornecedor.setNomeFantasia(resultSet.getString(3));
+                fornecedor.setTelefone(resultSet.getString(4));
+                fornecedor.setCep(resultSet.getString(5));
+                fornecedor.setRua(resultSet.getString(6));
+                fornecedor.setNumero(resultSet.getInt(7));
+                fornecedor.setBairro(resultSet.getString(8));
+                fornecedor.setCidade(resultSet.getString(9));
+                fornecedor.setEstado(resultSet.getString(10));
+                fornecedor.setComplemento(resultSet.getString(11));
+                fornecedor.setAnotacoes(resultSet.getString(12));
+                
                 fornecedores.add(fornecedor);
             }
             preparedStatement.close();
@@ -182,18 +216,7 @@ public class FornecedorDAO
         {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             
-            preparedStatement.setString(1, fornecedor.getCnpj());
-            preparedStatement.setString(2, fornecedor.getNome());
-            preparedStatement.setString(3, fornecedor.getNomeFantasia());
-            preparedStatement.setString(4, fornecedor.getCep());
-            preparedStatement.setString(5, fornecedor.getRua());
-            preparedStatement.setInt(6, fornecedor.getNumero());
-            preparedStatement.setString(7, fornecedor.getBairro());
-            preparedStatement.setString(8, fornecedor.getCidade());
-            preparedStatement.setString(9, fornecedor.getEstado());
-            preparedStatement.setString(10, fornecedor.getComplemento());
-            preparedStatement.setString(11, fornecedor.getAnotacoes());
-            preparedStatement.setString(12, cnpjAntigo);
+            preparedStatement.setString(1, cnpjFornecedor);
             
             preparedStatement.execute();
             preparedStatement.close();
